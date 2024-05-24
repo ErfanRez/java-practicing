e982748458ff564302d14fe069d275a1f1e0aab7
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-05-24 22:46:36 +0330

    full point solved questions

---
+++ code-star-1403/q1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String args[]) {
+      
+      Scanner input = new Scanner(System.in);
+      int k, n;
+
+      k = input.nextInt();
+      n = input.nextInt();
+      int[] a = new int[n];
+      
+      for (int i = 0 ; i < n ; i++) {
+          a[i] = input.nextInt();
+      }
+      
+      int sum = 0;
+      
+      for (int i = 0 ; i < n ; i++) {
+             if (i == 0) {
+                 if (a[i] <= k) {
+              sum += 2;
+          }else {
+              int temp = a[i];
+                  if ((temp % k) == 0) {
+                      sum += (int)(temp / k) + 1;
+                  } else {
+                      sum += (int) (temp / k) + 2;
+                  }
+          }
+                  
+                  
+              } else{
+                  int temp = Math.abs(a[i] - a[i -1]);
+                  if (a[i] <= k) {
+                      
+              sum += (temp / k) + 2;
+          } else {
+                  if ((temp % k) == 0) {
+                      sum += (int)(temp / k) + 1;
+                  } else {
+                      sum += (int)(temp / k) + 2;
+                  }
+          }
+                 
+              }
+      }
+      
+                if ((a[n-1] % k) == 0 ){
+                      sum += a[n-1] / k;
+                  } else {
+                      sum += a[n-1]/k + 1;
+                  }
+      
+      System.out.println(sum);
+    }
+}
