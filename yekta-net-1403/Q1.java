2d6e3ab2c9dba17a2d447574319576daa7275aaa
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-06-01 20:57:33 +0330

    code star solved questions

---
+++ yekta-net-1403/Q1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        String result = findNthString(n);
+        System.out.println(result.charAt(result.length() - 1));
+    }
+
+    private static String findNthString(int n) {
+      
+        LinkedList<String> queue = new LinkedList<>();
+        queue.add("a");
+        queue.add("b");
+
+        int count = 0;
+        String current = "";
+
+        while (count < n) {
+            current = queue.poll();
+            count++;
+            queue.add(current + "a");
+            queue.add(current + "b");
+        }
+
+        return current;
+    }
+}
