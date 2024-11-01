c72f7554c9d294b4082f433690bf9ccecd2b97f7
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-11-01 19:40:46 +0330

    New projects added

---
+++ OOP & DSA/Projects/ValidParentheses.java
@@ -0,0 +1,73 @@
+//! The following code is written by Erfan Rezaei
+//! Data Structures, Monday-Sec02, Student-ID: 40215041054114
+//! Islamic Azad University of Najafabad
+
+
+
+import java.util.*;
+
+public class validParentheses {
+  
+    public static String validParentheses(String s){
+      if(s.length() == 0) return "Unbalanced";
+      
+      Stack<Character> stack = new Stack<>();
+      
+      
+      for(char c : s.toCharArray()){
+        
+        if(c == '(' || c == '{' || c == '['){
+          stack.push(c);
+          
+        } else{
+          
+          if(stack.isEmpty()) return "Unbalanced";
+          
+          char top = stack.peek();
+          
+          if(
+              (c == ')' && top == '(') ||
+              (c == ']' && top == '[') ||
+              (c == '}' && top == '{')
+              
+            ) stack.pop();
+          
+          else return "Unbalanced";  
+        }
+        
+      }
+      
+      return stack.isEmpty() ? "Balanced" : "Unbalanced";
+    }
+  
+    public static void main(String[] args) {
+        System.out.println(validParentheses("((())"));         // Unbalanced
+        System.out.println(validParentheses("())(()"));        // Unbalanced
+        System.out.println(validParentheses("(()))()"));       // Unbalanced
+        System.out.println(validParentheses("(()())"));        // Balanced
+        System.out.println(validParentheses(")(("));           // Unbalanced
+        System.out.println(validParentheses("[()]"));          // Balanced
+        System.out.println(validParentheses("[(])"));          // Unbalanced
+        System.out.println(validParentheses("[()()]"));        // Balanced
+        System.out.println(validParentheses("{[({})[]]}"));    // Balanced
+        System.out.println(validParentheses("[()]{[()]()}"));  // Balanced
+        System.out.println(validParentheses("{()}[]"));        // Balanced
+        System.out.println(validParentheses("{(})[]"));        // Unbalanced
+        System.out.println(validParentheses("{()[]}"));        // Balanced
+        System.out.println(validParentheses("{})([]"));        // Unbalanced
+        System.out.println(validParentheses("({{}})[]()"));    // Balanced
+        System.out.println(validParentheses("()(()){([()])}")); // Balanced
+        System.out.println(validParentheses("((()(()){([()])}))")); // Unbalanced
+        System.out.println(validParentheses(")(()){([()])}")); // Unbalanced
+        System.out.println(validParentheses("({[])})"));       // Unbalanced
+        System.out.println(validParentheses("("));             // Unbalanced
+        System.out.println(validParentheses("{[()()]()}"));    // Balanced
+        System.out.println(validParentheses("{[()]}"));        // Balanced
+        System.out.println(validParentheses("{[(])}"));        // Unbalanced
+        System.out.println(validParentheses("[()]};"));        // Unbalanced
+        System.out.println(validParentheses("{[()]}"));        // Balanced
+        System.out.println(validParentheses("}("));            // Unbalanced
+        System.out.println(validParentheses("[[["));           // Unbalanced
+        System.out.println(validParentheses("[)]"));           // Unbalanced
+    }
+}
