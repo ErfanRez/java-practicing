0bbb95fc7be7e30aa6a60fdcfe26a7f96ac951b7
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-02-21 16:51:32 +0330

    Custom validations and new user form added

--- Spring/School-MVC/src/main/java/com/school/constant/AppConstants.java
+++ Spring/School-MVC/src/main/java/com/school/constant/AppConstants.java
@@ -1,7 +1,8 @@
 package com.school.constant;
 
 public class AppConstants {
-    public static final String ANONYMOUS = "Anonymous";
     public static final String OPEN = "Open";
     public static final String CLOSE = "Close";
+    public static final String STUDENT_ROLE = "STUDENT";
+    public static final String ADMIN_ROLE = "ADMIN";
 }
