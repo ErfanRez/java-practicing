9e6c719e47f779dc839a1560b278cf42dc667c0f
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-05-30 23:45:20 +0330

    Modified unbounded knapsack algorithm added

--- OOP-DSA/algorithms-practice/UnboundedKnapsack.java
+++ OOP-DSA/algorithms-practice/UnboundedKnapsack.java
@@ -1,22 +1,54 @@
-public class UnboundedKnapsack {
-    static int knapSack(int W, int[] val, int[] wt) {
-        int n = val.length;
-        int[] dp = new int[W + 1];
-        for (int j = 1; j <= W; j++) {
-            for (int i = 0; i < n; i++) {
-                if (wt[i] <= j) {
-                    dp[j] = Math.max(dp[j], dp[j - wt[i]] + val[i]);
+import java.util.Arrays;
+
+class UnboundedKnapsack {
+    static int n;
+    static int[] dp;
+    static int[] choice;
+    static int[] count;
+
+    static int knapsack(int W, int[] val, int[] wt) {
+        int n = wt.length;
+        dp = new int[W + 1];
+        choice = new int[W + 1];
+        Arrays.fill(choice, -1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = wt[i]; j <= W; j++) {
+                int newVal = dp[j - wt[i]] + val[i];
+                if (newVal > dp[j]) {
+                    dp[j] = newVal;
+                    choice[j] = i;
                 }
             }
         }
+
+        count = new int[n];
+        int rem = W;
+        while (rem > 0 && choice[rem] != -1) {
+            int item = choice[rem];
+            count[item]++;
+            rem -= wt[item];
+        }
+
         return dp[W];
     }
 
     public static void main(String[] args) {
-        int[] wt = {2, 3, 5, 6, 7};
+        int[] wt  = {1, 2, 5, 6, 7};
         int[] val = {1, 6, 18, 22, 28};
         int W = 11;
 
-        System.out.println("Maximum profit = " + knapSack(W, val, wt));
+        n = wt.length;
+
+        knapsack(W, val, wt);
+
+        System.out.println("Maximum value achievable = " + dp[W]);
+        System.out.println("Items chosen:");
+        for (int i = 0; i < n; i++) {
+            if (count[i] > 0) {
+                System.out.printf("  - Item %d (weight=%d, value=%d): %d pcs%n",
+                        i + 1, wt[i], val[i], count[i]);
+            }
+        }
     }
 }
